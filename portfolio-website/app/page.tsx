467ee4a23825733a5fca4db2380d9245d9e0f69d
shubhamnaikk <nshubham48@gmail.com>
2025-07-02 21:44:57 -0700

    Initial Project Setup

---
+++ portfolio-website/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black">
+      <div>
+        <h1>Welcome to My Portfolio</h1>
+        <p>
+          Hello! I'm Shubham Naik, a software engineer with a passion for
+          building innovative solutions. Explore my projects and skills below.
+        </p>
+      </div>
+    </main>
+  );
+}
