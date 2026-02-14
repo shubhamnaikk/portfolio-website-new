1767e824cfc5a37badd6ae67d00bc7cde6d8ba91
shubhamnaikk <nshubham48@gmail.com>
2026-02-14 10:41:30 -0800

    Added Projects

--- components/projects.tsx
+++ components/projects.tsx
@@ -34,10 +34,10 @@ const Projects = () => {
           <CardContainer>
             <CardBody className="group/card p-6 rounded-xl border border-black/10 dark:border-white/20 bg-gray-50 dark:bg-black">
               <CardItem translateZ="50" className="text-xl font-bold text-neutral-700 dark:text-white">
-                Agentic Natural Language Querying Across Structured Databases
+                Multi-Agentic Text-to-SQL system
               </CardItem>
               <CardItem as="p" translateZ="60" className="text-sm mt-2 text-neutral-500 dark:text-neutral-300">
-                A secure, robust, multi-agent Text-to-SQL system that allows users to query structured databases using natural language while guaranteeing safety, high accuracy, and error resilience.
+                A secure, robust, multi-agent Text-to-SQL system that allows users to query structured databases.
               </CardItem>
               <CardItem translateZ="100" className="w-full mt-4">
                 <Image
@@ -66,14 +66,14 @@ const Projects = () => {
           <CardContainer>
             <CardBody className="group/card p-6 rounded-xl border border-black/10 dark:border-white/20 bg-gray-50 dark:bg-black">
               <CardItem translateZ="50" className="text-xl font-bold text-neutral-700 dark:text-white">
-                ML Classifier API
+                Uber clone
               </CardItem>
               <CardItem as="p" translateZ="60" className="text-sm mt-2 text-neutral-500 dark:text-neutral-300">
-                Flask + Scikit-learn REST API for predicting outcomes.
+                Fullstack ride-hailing app built with Next.js, Tailwind CSS, and shadcn/ui components.
               </CardItem>
               <CardItem translateZ="100" className="w-full mt-4">
                 <Image
-                  src="https://media.geeksforgeeks.org/wp-content/uploads/20230315175228/Top-100-Machine-Learning-Projects-with-Source-Code-2.png"
+                  src="/2.png"
                   alt="Project 2"
                   width={600}
                   height={240}
@@ -84,7 +84,7 @@ const Projects = () => {
               <div className="flex justify-end mt-6">
                 <CardItem
                   as="a"
-                  href="https://yourproject2.com"
+                  href="https://github.com/shubhamnaikk/DATA236_Project_Group8.git"
                   target="_blank"
                   className="px-4 py-2 rounded-md bg-black text-white text-sm font-medium dark:bg-white dark:text-black hover:opacity-90 transition"
                 >
@@ -98,14 +98,14 @@ const Projects = () => {
           <CardContainer>
             <CardBody className="group/card p-6 rounded-xl border border-black/10 dark:border-white/20 bg-gray-50 dark:bg-black">
               <CardItem translateZ="50" className="text-xl font-bold text-neutral-700 dark:text-white">
-                Personal Portfolio
+                Uber eats Clone
               </CardItem>
               <CardItem as="p" translateZ="60" className="text-sm mt-2 text-neutral-500 dark:text-neutral-300">
                 Built using Next.js, Tailwind CSS, and shadcn/ui components.
               </CardItem>
               <CardItem translateZ="100" className="w-full mt-4">
                 <Image
-                  src="https://media.geeksforgeeks.org/wp-content/uploads/20230315175228/Top-100-Machine-Learning-Projects-with-Source-Code-2.png"
+                  src="/3.png"
                   alt="Project 3"
                   width={600}
                   height={240}
@@ -116,7 +116,7 @@ const Projects = () => {
               <div className="flex justify-end mt-6">
                 <CardItem
                   as="a"
-                  href="https://yourportfolio.com"
+                  href="https://github.com/shubhamnaikk/Data-236-UBER_EATS.git"
                   target="_blank"
                   className="px-4 py-2 rounded-md bg-black text-white text-sm font-medium dark:bg-white dark:text-black hover:opacity-90 transition"
                 >
@@ -130,14 +130,14 @@ const Projects = () => {
           <CardContainer>
             <CardBody className="group/card p-6 rounded-xl border border-black/10 dark:border-white/20 bg-gray-50 dark:bg-black">
               <CardItem translateZ="50" className="text-xl font-bold text-neutral-700 dark:text-white">
-                Data ETL Pipeline
+                Amazon Reviews Sentiment Analysis
               </CardItem>
               <CardItem as="p" translateZ="60" className="text-sm mt-2 text-neutral-500 dark:text-neutral-300">
-                Built with Apache Airflow, PostgreSQL and dbt for stock data.
+                End-to-end pipeline for performing sentiment analysis on Amazon product reviews
               </CardItem>
               <CardItem translateZ="100" className="w-full mt-4">
                 <Image
-                  src="https://media.geeksforgeeks.org/wp-content/uploads/20230315175228/Top-100-Machine-Learning-Projects-with-Source-Code-2.png"
+                  src="/4.png"
                   alt="Project 4"
                   width={600}
                   height={240}
@@ -148,7 +148,7 @@ const Projects = () => {
               <div className="flex justify-end mt-6">
                 <CardItem
                   as="a"
-                  href="https://yourproject4.com"
+                  href="https://github.com/shubhamnaikk/Sentiment-Analysis-on-Product-Reviews-Using-Apache-Spark-and-Hadoop.git"
                   target="_blank"
                   className="px-4 py-2 rounded-md bg-black text-white text-sm font-medium dark:bg-white dark:text-black hover:opacity-90 transition"
                 >
@@ -162,14 +162,14 @@ const Projects = () => {
           <CardContainer>
             <CardBody className="group/card p-6 rounded-xl border border-black/10 dark:border-white/20 bg-gray-50 dark:bg-black">
               <CardItem translateZ="50" className="text-xl font-bold text-neutral-700 dark:text-white">
-                Visual Analytics Tool
+                Real-Time Stock Market Data Processing Pipeline
               </CardItem>
               <CardItem as="p" translateZ="60" className="text-sm mt-2 text-neutral-500 dark:text-neutral-300">
-                Interactive D3.js-based dashboard for exploratory data analysis.
+                Pipeline covers data simulation, streaming, distributed storage, and querying capabilities.
               </CardItem>
               <CardItem translateZ="100" className="w-full mt-4">
                 <Image
-                  src="https://media.geeksforgeeks.org/wp-content/uploads/20230315175228/Top-100-Machine-Learning-Projects-with-Source-Code-2.png"
+                  src="/5.png"
                   alt="Project 5"
                   width={600}
                   height={240}
@@ -180,7 +180,7 @@ const Projects = () => {
               <div className="flex justify-end mt-6">
                 <CardItem
                   as="a"
-                  href="https://yourproject5.com"
+                  href="https://github.com/shubhamnaikk/Real-Time-Stock-Market-Data-Processing-Pipeline-Using-Apache-Kafka-and-AWS.git"
                   target="_blank"
                   className="px-4 py-2 rounded-md bg-black text-white text-sm font-medium dark:bg-white dark:text-black hover:opacity-90 transition"
                 >
@@ -194,14 +194,14 @@ const Projects = () => {
           <CardContainer>
             <CardBody className="group/card p-6 rounded-xl border border-black/10 dark:border-white/20 bg-gray-50 dark:bg-black">
               <CardItem translateZ="50" className="text-xl font-bold text-neutral-700 dark:text-white">
-                Chatbot with Rasa
+                Retail Store Analysis Power BI Dashboard
               </CardItem>
               <CardItem as="p" translateZ="60" className="text-sm mt-2 text-neutral-500 dark:text-neutral-300">
-                NLP chatbot trained on custom intents and integrated with APIs.
+                Data visualization tool designed to transform raw transactional data.
               </CardItem>
               <CardItem translateZ="100" className="w-full mt-4">
                 <Image
-                  src="https://media.geeksforgeeks.org/wp-content/uploads/20230315175228/Top-100-Machine-Learning-Projects-with-Source-Code-2.png"
+                  src="/6.png"
                   alt="Project 6"
                   width={600}
                   height={240}
@@ -212,7 +212,7 @@ const Projects = () => {
               <div className="flex justify-end mt-6">
                 <CardItem
                   as="a"
-                  href="https://yourproject6.com"
+                  href="https://github.com/shubhamnaikk/Retail-Store-Analysis-Power-BI-Dashboard.git"
                   target="_blank"
                   className="px-4 py-2 rounded-md bg-black text-white text-sm font-medium dark:bg-white dark:text-black hover:opacity-90 transition"
                 >
